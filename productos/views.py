f6363f7aa6da463a2771cbbe87ba03b81a7c7cb8
Naomi <solanch.veliz@tecsup.edu.pe>
2026-05-09 11:35:00 -0500

    implementar vistas CRUD con ModelViewSet

--- productos/views.py
+++ productos/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets, filters
 from .models import Producto, Proveedor
 from .serializers import ProductoSerializer, ProveedorSerializer
 
+
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['nombre']
+
 
 class ProveedorViewSet(viewsets.ModelViewSet):
     queryset = Proveedor.objects.all()
