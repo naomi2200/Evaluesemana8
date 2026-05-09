0d7597eb74541f7e9567c5dc917c4bd462d8caba
Naomi <solanch.veliz@tecsup.edu.pe>
2026-05-09 11:25:00 -0500

    agregar modelos Producto y Proveedor

--- productos/models.py
+++ productos/models.py
@@ -6,7 +6,6 @@ class Proveedor(models.Model):
 
     def __str__(self):
         return self.nombre
-
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
